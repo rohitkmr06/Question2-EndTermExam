7b7e12ca2910b8f8e33f7895891481b860baf19e
Rohit <Rohit@DESKTOP-71KACQD>
2019-07-30 00:38:40 +0530

    Complete!

--- src/main/java/main/Question2.java
+++ src/main/java/main/Question2.java
@@ -9,19 +9,19 @@ import java.util.TreeSet;
 public class Question2 {
 public static void main(String[] args) {
 	List<Integer> numbers = new ArrayList<Integer>();
-	Scanner sc = new Scanner(System.in);
+	Scanner s = new Scanner(System.in);
 	System.out.println("Enter the values: ");
-	int Length = sc.nextInt();
-	sc.nextLine();
+	int Length = s.nextInt();
+	s.nextLine();
 	for(int i=0;i<Length;i++)
-		numbers.add(Integer.parseInt(sc.nextLine()));
+		numbers.add(Integer.parseInt(s.nextLine()));
 	Question2 qns2=new Question2();
 	System.out.println(qns2.listWithDuplicates(numbers));
 
 }
 public List<Integer> listWithDuplicates(List<Integer> number){
 	List <Integer> L1 = null;
-	//write your code here !! change the L1 to required value
+	L1 = new ArrayList<Integer>(number);
 	return L1 ;
 }
 }
